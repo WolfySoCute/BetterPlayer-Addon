538b7a94f6ec4f0a9eff77e550c797b26ef1ba5a
wolfy <105298196+WolfySoCute@users.noreply.github.com>
2026-06-09 03:06:15 +0500

    - add class with logic for custom buttons settings

---
+++ src/player/controls.ts
@@ -0,0 +1,72 @@
+import {FULLSCREEN_PLAYER, PLAYER_CONTROLS} from '@/player/constants';
+import {
+    ButtonSize, ContextMenuButton,
+    LikeButton,
+    LyricsButton,
+    NextButton,
+    PlayButton,
+    PrevButton, QueueButton,
+    RepeatButton, ShuffleButton,
+} from '@/elements/controlButton';
+
+export class Controls {
+    get fullscreenPlayerDiv(): HTMLDivElement | null {
+        if (!document.body) {
+            return null;
+        }
+
+        return document.querySelector(FULLSCREEN_PLAYER);
+    }
+
+    public hideCover(isHidden: boolean) {
+        const posterDiv = document.querySelector(PLAYER_CONTROLS.posterContent);
+        if (isHidden) posterDiv?.classList.add('hidden');
+        else posterDiv?.classList.remove('hidden');
+    }
+
+    private createControlsDiv() {
+        const controlsDiv = document.createElement('div');
+
+        const queueButton = new QueueButton(ButtonSize.SMALL);
+        const contextMenuButton = new ContextMenuButton(ButtonSize.SMALL);
+        const shuffleButton = new ShuffleButton(ButtonSize.SMALL);
+        const prevButton = new PrevButton(ButtonSize.MEDIUM);
+        const playButton = new PlayButton(ButtonSize.MEDIUM);
+        const nextButton = new NextButton(ButtonSize.MEDIUM);
+        const repeatButton = new RepeatButton(ButtonSize.SMALL);
+        const likeButton = new LikeButton(ButtonSize.SMALL)
+        const lyricsButton = new LyricsButton(ButtonSize.SMALL);
+
+        controlsDiv.classList.add('customControls');
+        controlsDiv.appendChild(queueButton.element);
+        controlsDiv.appendChild(contextMenuButton.element);
+        controlsDiv.appendChild(shuffleButton.element);
+        controlsDiv.appendChild(prevButton.element);
+        controlsDiv.appendChild(playButton.element);
+        controlsDiv.appendChild(nextButton.element);
+        controlsDiv.appendChild(repeatButton.element);
+        controlsDiv.appendChild(likeButton.element);
+        controlsDiv.appendChild(lyricsButton.element);
+
+        return controlsDiv;
+    }
+
+    public setCustomControls(controlsEnabled: boolean) {
+        const fullscreenPlayerDiv = this.fullscreenPlayerDiv;
+        if (!fullscreenPlayerDiv) return;
+
+        const customControls = fullscreenPlayerDiv.querySelector('.customControls');
+        if (controlsEnabled && customControls !== null) return;
+
+        if (controlsEnabled) {
+            const timecodeWrapper = fullscreenPlayerDiv.querySelector(PLAYER_CONTROLS.timecodeWrapper);
+            const playerControls = timecodeWrapper?.parentElement;
+
+            playerControls?.appendChild(this.createControlsDiv());
+        } else {
+            if (customControls !== null) {
+                customControls.remove();
+            }
+        }
+    }
+}
