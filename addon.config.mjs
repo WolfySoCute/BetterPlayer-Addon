7cab92a72fe9f50ce756542704041b643e1bfa31
wolfy <105298196+WolfySoCute@users.noreply.github.com>
2026-06-09 02:58:19 +0500

    - update supported version - change version

--- addon.config.mjs
+++ addon.config.mjs
@@ -3,7 +3,7 @@ const addonConfig = {
     directoryName: 'betterplayer',
     name: 'BetterPlayer',
     description: 'Кастомизация для стандартного полноэкранного плеера',
-    version: '1.6.0',
+    version: '1.6.0-alpha-2',
     author: ['WolfySoCute', 'forea.adoxid'],
     type: 'script',
     image: 'image.png',
@@ -12,7 +12,7 @@ const addonConfig = {
     tags: ['customization', 'player'],
     dependencies: [],
     allowedUrls: ['https://i.ibb.co/'],
-    supportedVersions: ['5.90.0'],
+    supportedVersions: ['5.97.3'],
 }
 
 export default addonConfig
