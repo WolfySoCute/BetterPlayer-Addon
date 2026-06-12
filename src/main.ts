37b3c02187b0f16881f102a039e7588ef0dec7e2
wolfy <105298196+WolfySoCute@users.noreply.github.com>
2026-06-12 16:51:26 +0500

    Разделение стилей по файлам

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import './styles.css'
+import './styles/additional.css';
+import './styles/additionalContent.css';
+import './styles/background.css';
+import './styles/customControls.css';
+import './styles/customControlsThemes.css';
 
 import {mountPlayer} from '@/player';
 
