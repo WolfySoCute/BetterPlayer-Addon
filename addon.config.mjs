bc0384d8fb290731910b9c0b93c042998360c52c
wolfy <105298196+WolfySoCute@users.noreply.github.com>
2026-06-12 16:04:57 +0500

    Исправлена ошибка в поддерживаемой версии яндекс музыки в конфиге

--- addon.config.mjs
+++ addon.config.mjs
@@ -3,7 +3,7 @@ const addonConfig = {
     directoryName: 'betterplayer',
     name: 'BetterPlayer',
     description: 'Кастомизация для стандартного полноэкранного плеера',
-    version: '1.6.0',
+    version: '1.6.1',
     author: ['WolfySoCute', 'forea.adoxid'],
     type: 'script',
     image: 'image.png',
@@ -12,7 +12,7 @@ const addonConfig = {
     tags: ['customization', 'player'],
     dependencies: [],
     allowedUrls: ['https://i.ibb.co/', 'https://i.postimg.cc/', 'https://files.catbox.moe/'],
-    supportedVersions: ['5.97.3'],
+    supportedVersions: ['>=5.90.0'],
 }
 
 export default addonConfig
